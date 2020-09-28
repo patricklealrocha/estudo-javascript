d1d1e38176d5b8680d077157bd1a25dfe0da3d09
Patrick Dias Leal Rocha <pdiasrocha@yahoo.com.br>
2020-09-28 16:13:48 -0300

    ajustando datas

--- projeto-calculadora-clone/scripts/controller/CalcController.js
+++ projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -3,35 +3,58 @@ class CalcController {
     
     constructor(){
 
-        this._displayCalc = '0';
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEL = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate ;
         this.initialize();
     }
     // método principal do projeto
     initialize(){
-        let displayCalcEl = document.querySelector("#display");
-        let dateEL = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
 
-        displayCalcEl.innerHTML = "4567";
-        dateEL.innerHTML = "01/05/2020";
-        timeEl.innerHTML = "00:00";
+        this.setdisplayDateTime();
 
+        setInterval(() => {
+            this.setdisplayDateTime();       
+        }, 1000)         
+    }
+    //Método para iniciar a calculadora e manter os intervalos
+    setdisplayDateTime(){
+        
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+    // get e set display time
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+    
+    // get e set display date
+    get displayDate(){
+        return this._dateEL.innerHTML;
+    }
+    set displayDate(value){
+        return this._dateEL.innerHTML = value;
     }
 
+    // get e set display calc
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
     }
 
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    // get e set data atual
+    get currentDate(){
+        return new Date();
     }
-
-    get dataAtual(){
-        return this._currentDate;
-    }
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(value){
+        this._currentDate = value;
     }
 
 
