1cf23652fbec2cbb20d0236937e493e4770589f9
Patrick Dias Leal Rocha <pdiasrocha@yahoo.com.br>
2020-09-28 21:56:39 -0300

    tratando eventos

--- projeto-calculadora-clone/scripts/controller/CalcController.js
+++ projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -9,6 +9,7 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate ;
         this.initialize();
+        this.initButtonsEvents();
     }
     // método principal do projeto
     initialize(){
@@ -18,6 +19,29 @@ class CalcController {
         setInterval(() => {
             this.setdisplayDateTime();       
         }, 1000)         
+    }
+    // Aplicando vários eventos
+    addEventListenerAll(element , events, fn){
+
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        })
+
+    }
+    
+    //Controlando eventos com querySector
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn, 'click drag', e =>{
+                console.log(btn.className.baseVal.replace("btn-",""))
+            });
+            this.addEventListenerAll(btn , 'mouseover mouseup mousedown', e =>{
+                btn.style.cursor = 'pointer';
+            });
+        });
+
     }
     //Método para iniciar a calculadora e manter os intervalos
     setdisplayDateTime(){
